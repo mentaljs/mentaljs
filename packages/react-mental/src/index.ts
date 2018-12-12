33dc0c4a74e24ed659fc7c98587cfb913143767a
Steve Kite <steve@statecraft.one>
2018-12-12 16:20:35 +0300

    Unify dependencies

--- packages/react-mental/src/index.ts
+++ packages/react-mental/src/index.ts
@@ -1,5 +1,5 @@
-import './styles.css';
 export { XView, XViewProps } from './XView';
+export { XStyles, calculateStyles, XStyleFactory, XStyleFactoryRegistry } from 'mental-styles';
 export { XViewSelectedContext } from './XViewSelectedContext';
 export { XViewRouter, XViewRoute } from './XViewRouter';
 export { XViewRouterContext } from './XViewRouterContext';
